2a9b9eadaa2cc1724767cf8e8722e6715190ecf5
Nunny R <nunnyreyes@gmail.com>
2020-09-11 10:31:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+//writeCards= ["Ada", "Brendan", "Ali"];
+
+function writeCards (names, birthday) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+    thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`)
+    }
+return thankYouCards
+}
+
+function countDown(top) {
+    while (top > 0) {
+    console.log(top);
+    top--
+    }
+    console.log(0)
+}
