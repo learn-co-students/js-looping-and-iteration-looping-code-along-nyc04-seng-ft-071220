0b5da81543e9966b57397ebc24400f30e7deb94c
Christy Tropila <ctropilalakebeverage@gmail.com>
2020-09-13 19:06:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(name, giftType){
+  let thankYou=[]
+  for(let i=0; i<name.length; i++){
+  thankYou.push(`Thank you, ${name[i]}, for the wonderful ${giftType} gift!`)
+  }
+  return thankYou;
+}
+
+function countDown(num){
+while(num > 0){
+console.log(num);
+num -= 1;
+}
+console.log(num);
+}
