13d75d3971644de43abe91d9cc828184d2007ec7
wlcreate <waverley.nyc@gmail.com>
2020-09-14 14:48:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+        thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!` )
+    }
+    return thankYouCards;    
+}
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer);
+}
