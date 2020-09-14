9bb0b6d6d8302fe14e75fa9e03d9e72db20022b1
fbado66 <fbado66@gmail.com>
2020-09-13 21:11:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// writeCards(["Ada", "Brendan", "Ali"], 'surprise gift');
+
+function writeCards(names, surprise) {
+    let finalArr = []
+    for (let i = 0; i < names.length; i++ ){
+        finalArr.push( `Thank you, ${names[i]}, for the wonderful ${surprise} gift!` )
+    }
+    return finalArr   
+}
+
+function countDown( numbers) {
+    while (numbers > 0) {
+        console.log(numbers);
+        numbers -= 1;
+
+    }
+    console.log (numbers);
+}
